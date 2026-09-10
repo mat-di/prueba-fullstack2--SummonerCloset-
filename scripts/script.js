dea9d0727a46d2c8dd0a87c29b0698dd78b3c822
RappCL <ra.palestro@duocuc.cl>
2026-09-10 06:46:17 -0300

    Preview skin campeon
    Se moldea base para futuro display de skin de los campeones (Falta mucho por perfeccionar) + Ashe Canción del océano

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 const limpiar = document.getElementById('limpiar-filtros');
 const buscadorInput = document.getElementById('buscador-input');
+const rol_top = document.getElementById('top');
+const rol_jungle = document.getElementById('jungle');
+const rol_mid = document.getElementById('mid');
+const rol_bot = document.getElementById('bot');
+const rol_supp = document.getElementById('supp');
 
 function limpiarFiltros(){
     buscadorInput.value = '';
